b04870126209fc58075b3a0dfc5f18e1d62b432b
Ozimnadius <79251486565@yandex.ru>
2017-03-08 19:01:34 +0300

    Page1 100%, Page2 100%, Page3 70%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $('#fullpage').fullpage({
     anchors:['Page1', 'Page2', 'Page3', 'Page4',
         'Page5', 'Page6', 'Page7'],
-    menu: '#mainMenu'
+    menu: '#mainMenu',
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom'
 });
